8ccd78189126f86f2a6bf1a60ef6af9ecdaaa812
tales <talescmoreira@gmail.com>
2021-10-30 13:20:49 -0300

    Criaçao da model Evento

---
+++ back/src/models/EventoModel.js
@@ -0,0 +1,11 @@
+const DataTypes = require("sequelize");
+const sequelize = require();
+
+const Evento = sequelize.define('Evento', {
+        conteudo:{
+            type:DataTypes.STRING,
+            allowNull = true
+        },
+        
+
+});
