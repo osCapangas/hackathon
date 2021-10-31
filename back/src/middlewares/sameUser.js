ce3dbf113953c385bd0396815c7fdebe66e364d7
tales <talescmoreira@gmail.com>
2021-10-31 12:13:04 -0300

    bugfix: aparecer as middlewares

--- back/src/middlewares/sameUser.js
+++ back/src/middlewares/sameUser.js
@@ -1,1 +1,15 @@
-//TODO middleware 
+const Role = require('../models/RoleModel');
+
+const sameUser = async(req, res, next) => {
+    const {id} = req.params
+    try{
+        const role = await Role.findOne({where : {UserId: id}});
+        if (role.name == 'admin' || req.params == req.body.UserId){ 
+            return next();}
+        else return res.status(401).json({'error' : 'Sem autorização'});
+    } catch (e){
+        return res.status(500).json(e + "!");
+    };
+}
+
+module.exports = sameUser;
